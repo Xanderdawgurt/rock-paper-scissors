912b09a620617abebcf9edbb1561d042d439bfd5
Xander <xdancel06@gmail.com>
2025-08-15 00:25:23 -0500

    Complete and finish game

--- index.js
+++ index.js
@@ -5,6 +5,8 @@
 // In this function create rng with Math.random * 100
 // split the numbers equally into three groups then assign rock, paper, or scissors
 // then test with console.log 
+
+
 function getComputerChoice(choice) {
     let randomNumber = Math.random();
     let randomNumberTwo = randomNumber * 100;
@@ -22,20 +24,23 @@ function getComputerChoice(choice) {
 
 }
 let ComputerChoice = getComputerChoice();
-console.log(ComputerChoice);
+// console.log(ComputerChoice);
+
 // Create a new function named getHumanChoice 
 // set prompt to ask for Input 
 // return input 
+
 function getHumanChoice() {
     let userInput = prompt("Pick: Rock, Paper, or Scissors");
     return userInput;
 }
 let HumanChoice = getHumanChoice();
-console.log(HumanChoice);
+// console.log(HumanChoice);
+
 // Declare two variables for humanScore and computerScore with a starting value of 0
 
-// let humanScore = 0; 
-// let computerScore = 0;
+let humanScore = 0; 
+let computerScore = 0;
 
 // create new function named playRound
 // the two parameters will be humanChoice and getComputerChoice
@@ -65,23 +70,54 @@ console.log(HumanChoice);
 // declare playRound function and score variables so theyre inside playGame
 // write if else statement first score to 5, wins
 
-function playGame() {
-    let humanScore = 0; 
-    let computerScore = 0;
+
 
     function playRound(theHumanChoice, theComputerChoice) {
+
+        function getHumanChoice() {
+        let userInput = prompt("Pick: Rock, Paper, or Scissors");
+        return userInput;
+        }
+        let HumanChoice = getHumanChoice();
+        console.log(HumanChoice);
+
+        function getComputerChoice(choice) {
+    let randomNumber = Math.random();
+    let randomNumberTwo = randomNumber * 100;
+    let randomNumberThree = (Math.floor(randomNumberTwo));
+
+    if (randomNumberThree <= 33) {
+        choice = "rock";
+    } else if (randomNumberThree <= 66) {
+        choice = "paper";
+    } else {
+        choice = "scissors";
+    }
+
+    return choice;
+
+}
+let ComputerChoice = getComputerChoice();
+console.log(ComputerChoice);
+
         theHumanChoice = HumanChoice.toLowerCase(); 
         theComputerChoice = ComputerChoice; 
 
         if (theHumanChoice == theComputerChoice) {
         console.log("You tied, loser.");
-        } else if (theHumanChoice == "rock" && theComputerChoice == "scissor" || theHumanChoice == "scissor" && theComputerChoice == "paper" || theHumanChoice == "paper" && theComputerChoice == "rock") {
-    console.log("You win!");
-    ++humanScore; 
+        } else if (theHumanChoice == "rock" && theComputerChoice == "scissors" || theHumanChoice == "scissors" && theComputerChoice == "paper" || theHumanChoice == "paper" && theComputerChoice == "rock") {
+        console.log("You win!");
+        ++humanScore; 
+        console.log("Your score", humanScore);
+        console.log("Computer score", computerScore);
         } else {
         console.log("You lose!");
         ++computerScore; 
+        console.log("Your score", humanScore);
+        console.log("Computer score", computerScore);
         }
+    }
+
 
         while (humanScore < 5 || computerScore < 5) {
             playRound(HumanChoice, ComputerChoice); 
@@ -94,10 +130,6 @@ function playGame() {
                 break;
             }
         }
-    }
-}
-
-
 
 //This code threw an error because the loop needed to be outside the code
 // let TheFinalGame = playGame(); 
